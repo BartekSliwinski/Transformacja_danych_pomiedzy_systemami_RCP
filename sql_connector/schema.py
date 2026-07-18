c9ff65e060886f6fb85caa78e7e2be6a4506ce97
BartekSliwinski <sliwinski.bartek03@gmail.com>
2026-07-18 16:35:12 +0200

    Tworzenie tabeli działów

--- sql_connector/schema.py
+++ sql_connector/schema.py
@@ -1,13 +1,14 @@
-
-
-def create_departments_table():
-    create_departments = text(
+def create_departments_table(connection):
+    create_departments_query = text(
         """
-            IF OBJECT_ID('dbo.Departments', 'U') IS NULL
-            BEGIN
-                CREATE TABLE dbo.Depratments (
-                    ID
-                )
-
+        IF OBJECT_ID('dbo.Departments', 'U') IS NULL
+        BEGIN
+            CREATE TABLE dbo.Departments (
+                DepartmentID INT IDENTITY(1,1) PRIMARY KEY,
+                DepartmentName VARCHAR(30) NOT NULL UNIQUE
+            );
+        END
         """
     )
+    connection.execute(create_departments_query)
+    connection.commit()
